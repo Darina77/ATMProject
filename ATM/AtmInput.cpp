e2b51a0806f82d3b143841ed618a8155a3cf3934
Darina77 <darina.pishchik@gmail.com>
2017-11-19 16:45:36 +0200

    Chenges!

--- ATM/AtmInput.cpp
+++ ATM/AtmInput.cpp
@@ -1,8 +1,24 @@
 #include "atminput.h"
 #include <QCoreApplication>
+#include <QUiLoader>
+#include <QFile>
+#include <QVBoxLayout>
+#include <QRect>
 
 AtmInput::AtmInput(QWidget *parent)
    : QMainWindow(parent)
 {
-     ui.setupUi(this);
+    QUiLoader loader;
+    QFile file(":/forms/login.ui");
+    file.open(QFile::ReadOnly);
+    QWidget *myWidget = loader.load(&file, this);
+    file.close();
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(myWidget);
+    setLayout(layout);
+
+    resize(myWidget->size());
+    setMaximumSize(myWidget->size());
+    setMinimumSize(myWidget->size());
 }
